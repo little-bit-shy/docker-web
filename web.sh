61fdb1eaecc2bc3494b783d24cf4246a626676e4
xuguozhi <1533356676@qq.com>
2018-07-03 17:48:23 +0800

    脚本优化

--- web.sh
+++ web.sh
@@ -8,10 +8,10 @@ docker pull php:7.2.7-fpm-rewrite
 docker rm $(docker ps -a| grep "php" |cut -d " " -f 1) -f
 docker run -d --name php --net=host  \
 	-v ./php/www:/www \
-	-v ./php/conf/php.ini:/usr/local/etc/php/php.ini \
-	-v ./php/conf/php-fpm.conf:/usr/local/etc/php-fpm.conf \
-	-v ./php/conf/php-fpm.d/www.conf:/usr/local/etc/php-fpm.d/www.conf \
-	-v ./php/logs:/phplogs \
+	-v /usr/local/docker/php/conf/php.ini:/usr/local/etc/php/php.ini \
+	-v /usr/local/docker/php/conf/php-fpm.conf:/usr/local/etc/php-fpm.conf \
+	-v /usr/local/docker/php/conf/php-fpm.d/www.conf:/usr/local/etc/php-fpm.d/www.conf \
+	-v /usr/local/docker/php/logs:/phplogs \
 	php:7.2.7-fpm-rewrite
  
 ############################Nginx
@@ -19,8 +19,8 @@ docker run -d --name php --net=host  \
 docker pull nginx
 docker rm $(docker ps -a| grep "nginx" |cut -d " " -f 1) -f
 docker run -d --name nginx --net=host \
-	-v ./nginx/conf/nginx.conf:/etc/nginx/nginx.conf \
-	-v ./nginx/conf/conf.d:/etc/nginx/conf.d \
-	-v ./nginx/logs:/var/log/nginx \
-	-v ./nginx/html:/var/www/html \
+	-v /usr/local/docker/nginx/conf/nginx.conf:/etc/nginx/nginx.conf \
+	-v /usr/local/docker/nginx/conf/conf.d:/etc/nginx/conf.d \
+	-v /usr/local/docker/nginx/logs:/var/log/nginx \
+	-v /usr/local/docker/nginx/html:/var/www/html \
 	nginx
