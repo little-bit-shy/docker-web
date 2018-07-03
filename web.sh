7f762c99f4119675bcef883c7eef7cc5c6b31622
xuguozhi <1533356676@qq.com>
2018-07-03 17:55:29 +0800

    脚本优化

--- web.sh
+++ web.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 #web环境部署
 
+dir="/usr/local/docker"
+[ -d "$dir" ] && rmdir "$dir"
+mkdir "$dir"
+mv ./php ${dir}
+mv ./nginx ${dir}
+
 #############################Php
 docker build -t php:7.2.7-fpm-rewrite ./php 
 
@@ -8,10 +14,10 @@ docker pull php:7.2.7-fpm-rewrite
 docker rm $(docker ps -a| grep "php" |cut -d " " -f 1) -f
 docker run -d --name php --net=host  \
 	-v ./php/www:/www \
-	-v /usr/local/docker/php/conf/php.ini:/usr/local/etc/php/php.ini \
-	-v /usr/local/docker/php/conf/php-fpm.conf:/usr/local/etc/php-fpm.conf \
-	-v /usr/local/docker/php/conf/php-fpm.d/www.conf:/usr/local/etc/php-fpm.d/www.conf \
-	-v /usr/local/docker/php/logs:/phplogs \
+	-v ${dir}/php/conf/php.ini:/usr/local/etc/php/php.ini \
+	-v ${dir}/php/conf/php-fpm.conf:/usr/local/etc/php-fpm.conf \
+	-v ${dir}/php/conf/php-fpm.d/www.conf:/usr/local/etc/php-fpm.d/www.conf \
+	-v ${dir}/php/logs:/phplogs \
 	php:7.2.7-fpm-rewrite
  
 ############################Nginx
@@ -19,8 +25,8 @@ docker run -d --name php --net=host  \
 docker pull nginx
 docker rm $(docker ps -a| grep "nginx" |cut -d " " -f 1) -f
 docker run -d --name nginx --net=host \
-	-v /usr/local/docker/nginx/conf/nginx.conf:/etc/nginx/nginx.conf \
-	-v /usr/local/docker/nginx/conf/conf.d:/etc/nginx/conf.d \
-	-v /usr/local/docker/nginx/logs:/var/log/nginx \
-	-v /usr/local/docker/nginx/html:/var/www/html \
+	-v ${dir}/nginx/conf/nginx.conf:/etc/nginx/nginx.conf \
+	-v ${dir}/nginx/conf/conf.d:/etc/nginx/conf.d \
+	-v ${dir}/nginx/logs:/var/log/nginx \
+	-v ${dir}/nginx/html:/var/www/html \
 	nginx
