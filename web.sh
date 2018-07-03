e9bb698a4100f298b215fa80da11c93cd1055088
xuguozhi <1533356676@qq.com>
2018-07-03 16:05:21 +0800

    脚本优化

--- web.sh
+++ web.sh
@@ -9,6 +9,8 @@ docker rm $(docker ps -a| grep "php" |cut -d " " -f 1) -f
 docker run -d --name php --net=host  \
 	-v /usr/local/docker/php/www:/www \
 	-v /usr/local/docker/php/conf/php.ini:/usr/local/etc/php/php.ini \
+	-v /usr/local/docker/php/conf/php-fpm.conf:/usr/local/etc/php-fpm.conf \
+	-v /usr/local/docker/php/conf/php-fpm.d/www.conf:/usr/local/etc/php-fpm.d/www.conf \
 	-v /usr/local/docker/php/logs:/phplogs \
 	php:7.2.7-fpm-rewrite
  
