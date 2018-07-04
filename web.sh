33e7bb2e484b3a020b9449d07c0dfa4545c679b3
xuguozhi <1533356676@qq.com>
2018-07-04 10:06:47 +0800

    脚本优化

--- web.sh
+++ web.sh
@@ -2,7 +2,7 @@
 #web环境部署
 
 read -p "application dir:" dir
-([ -d "$dir" ] || mkdir "$dir") || (echo "the application dir is fail!" && exit)
+([ -d "$dir" ] || mkdir "$dir") || (echo "the application dir is fail!" && exit 1)
 mv -i ./php ${dir}
 mv -i ./nginx ${dir}
 
