5f03807cbe93f17520ec78aa9acccec0e444a85a
xuguozhi <1533356676@qq.com>
2018-07-04 10:32:42 +0800

    脚本优化

--- web.sh
+++ web.sh
@@ -7,8 +7,8 @@ if [ ! -d "$dir" ];then
     echo "the application dir is fail!"
     exit 2
 fi
-mv -i ./php ${dir}
-mv -i ./nginx ${dir}
+mv -f ./php ${dir}
+mv -f ./nginx ${dir}
 
 #############################Php
 docker build -t php:7.2.7-fpm-rewrite ${dir}/php
