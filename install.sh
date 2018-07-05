5f36fcf5213ab566d316555189057f51d9aa1315
xuguozhi <1533356676@qq.com>
2018-07-05 17:04:35 +0800

    脚本优化

--- install.sh
+++ install.sh
@@ -4,11 +4,14 @@
 read -p "application dir:" dir
 [ -d "$dir" ] || mkdir "$dir"
 if [ ! -d "$dir" ];then
-    echo "the application dir is fail!"
+    echo -e "\033[31m the application dir is fail! \033[0m"
     exit 2
 fi
 /bin/cp -fr ./php ${dir}
 /bin/cp -fr ./nginx ${dir}
 /bin/cp -fr ./mysql ${dir}
 /bin/cp -fr ./redis ${dir}
-/bin/cp -fr ./run.sh ${dir}
+/bin/cp -fr ./run ${dir}
+
+echo -e "\033[32m install success! \033[0m"
+exit 0
