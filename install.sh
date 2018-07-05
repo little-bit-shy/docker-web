3ee20b8648df857a54143f27ec896f4f849d4a36
xuguozhi <1533356676@qq.com>
2018-07-05 17:06:16 +0800

    脚本优化

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ if [ ! -d "$dir" ];then
     echo -e "\033[31m the application dir is fail! \033[0m"
     exit 2
 fi
+
 /bin/cp -fr ./php ${dir}
 /bin/cp -fr ./nginx ${dir}
 /bin/cp -fr ./mysql ${dir}
@@ -14,4 +15,4 @@ fi
 /bin/cp -fr ./run ${dir}
 
 echo -e "\033[32m install success! \033[0m"
-exit 0
+exit 1
